adc71d4b33deb6dcaf319c31473d7baa536775d4
mohammed-eddaghal <med.eddaghal@gmail.com>
2021-03-30 23:26:57 +0100

    add la fonction calcule distance entre 2 points dans la carte google maps

--- app/src/main/java/com/ensias/mine_is_yoursapp/MapsActivity.java
+++ app/src/main/java/com/ensias/mine_is_yoursapp/MapsActivity.java
@@ -10,7 +10,7 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.widget.Toast;
+
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -18,11 +18,11 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.maps.android.SphericalUtil;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -30,6 +30,7 @@ public class MapsActivity extends FragmentActivity {
     private FusedLocationProviderClient client;
     private SupportMapFragment mapFragment;
     private LocationManager locationManager;
+    private LatLng latLng;
 
     private static final int locationRequestCode = 1000;
     private double wayLatitude = 0.0, wayLongitude = 0.0;
@@ -37,6 +38,7 @@ public class MapsActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         mapFragment = (SupportMapFragment) getSupportFragmentManager()
@@ -54,11 +56,22 @@ public class MapsActivity extends FragmentActivity {
         } else {
             // already permission granted
             getLocation();
+            /*LatLng latLng2 = new LatLng(34.0492, -6.7353);
+            if(latLng!=null)
+            Log.d("testYYY","latitude"+latLng.latitude+"longitude"+ latLng.longitude);
+            else
+                Log.d("testXX","latLng est null");
+*/
+            //Log.d("distance","la distance entre les deux point est : " + getDistance(latLng,latLng2));
 
         }
     }
 
-    private void getLocation() {
+    public int getDistance(LatLng l1,LatLng l2){
+        return (int) SphericalUtil.computeDistanceBetween(l1, l2);
+    }
+
+    private LatLng getLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -67,7 +80,7 @@ public class MapsActivity extends FragmentActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            return;
+            return null;
         }else{
             Log.d("test","test access to methode");
         client.getLastLocation().
@@ -81,32 +94,24 @@ public class MapsActivity extends FragmentActivity {
                         public void onMapReady(GoogleMap googleMap) {
                             Log.d("test3","latitude"+location.getLatitude()+"longitude"+ location.getLongitude());
                             //initialize lat lng
-                            LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            latLng = new LatLng(location.getLatitude(),location.getLongitude());
+                            LatLng latLng2 = new LatLng(34.0686, -6.7480);
+                            Log.d("distance","la distance est :" +getDistance(latLng,latLng2)/ 1000);
+
                             //creat marker potion
                             mMap = googleMap;
                             mMap.addMarker(new MarkerOptions().position(latLng).title("current location"));
+                            mMap.addMarker(new MarkerOptions().position(latLng2).title("location de test"));
 
                             mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
                         }
                     });
-                }else{
-                    mapFragment.getMapAsync(new OnMapReadyCallback() {
-                        @Override
-                        public void onMapReady(GoogleMap googleMap) {
-                            Log.d("test4","test access to methode 4");
-                            //initialize lat lng
-                            //LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
-                            LatLng latLng = new LatLng(33.975295730407225, -6.730296980371704);
-                            //creat marker potion
-                            mMap = googleMap;
-                            mMap.addMarker(new MarkerOptions().position(latLng).title("location de test"));
 
-                            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
-                        }
-                    });
                 }
             }
+
         });
+        return latLng;
         }
     }
 
